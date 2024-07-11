9a7bae10a09d63d45952c31521c71fce0027ce24
hansolkim9 <masicer2@gmail.com>
2024-07-11 12:43:58 +0900

    # 입력값 검증 이메일

--- src/components/auth/EmailInput.js
+++ src/components/auth/EmailInput.js
@@ -23,7 +23,7 @@ const EmailInput = () => {
 
     // 이메일 검증 후속 처리
     const checkEmail = async (email) => {
-        if (!emailValid) {
+        if (!validateEmail(email)) {
             // 에러메세지 세팅
             setError('이메일 형식이 유효하지 않습니다.');
             return;
@@ -42,11 +42,11 @@ const EmailInput = () => {
 
     const changeHandler = e => {
         const email = e.target.value;
-        const isValid = validateEmail(email);
+        // const isValid = validateEmail(email);
         // console.log('isValid: ', isValid);
 
         setEnteredEmail(email);
-        setEmailValid(isValid);
+        // setEmailValid(isValid);
 
         // 이메일 검증 후속처리
         checkEmail(email);
