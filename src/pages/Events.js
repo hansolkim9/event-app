476c2c400d8d774d80de6ea21c8dd7f280dc20d0
hansolkim9 <masicer2@gmail.com>
2024-07-17 23:30:21 +0900

    # 이벤트 생성 권한처리

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useContext } from 'react';
 import EventList from '../components/EventList';
 import EventSkeleton from '../components/EventSkeleton';
-
-// npm install loadsh
-import { debounce, throttle } from 'lodash';
-import {EVENT_URL} from "../config/host-config";
-import {useRouteLoaderData} from "react-router-dom";
+import { EVENT_URL } from '../config/host-config';
+import { useRouteLoaderData } from 'react-router-dom';
+import EventContext from '../components/context/event-context';
 
 const Events = () => {
+
+    const { changeTotalEventCount } = useContext(EventContext);
 
     const {token} = useRouteLoaderData('user-data');
 
@@ -50,6 +50,9 @@ const Events = () => {
             headers: { 'Authorization': `Bearer ${token}` },
         });
         const { events: loadedEvents, totalCount } = await response.json();
+
+        // 전역 상태값 변경
+        changeTotalEventCount(totalCount);
 
         console.log('loaded: ', { loadedEvents, totalCount, len: loadedEvents.length });
 
